eb9890ece2a6424f8b47e91be9d7a82da8b71fe6
Jhoset Andres Llacchua Sales <85779947+MonsterCrak@users.noreply.github.com>
2026-06-29 11:42:16 -0500

    Initial commit: Spring Boot academic management API

---
+++ src/test/java/edu/upc/sistema/gestionacademicaapi/GestionAcademicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.upc.sistema.gestionacademicaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAcademicaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
